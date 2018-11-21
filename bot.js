611fa3e25718dabf1d0fc971e9e5b845e09bf18c
iMoomG <45240593+iMoomG@users.noreply.github.com>
2018-11-21 22:28:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,10 +7,10 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "509357857666236430"; // ايدي السررفر
-var channel = "509357857666236434";//ايدي الروم
+var server = "509357857666236430"; // 514855711830835202
+var channel = "509357857666236434"; // 514855712380157959
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
+    client.guilds.get(server).channels.get(channel).send('**GAM , GAM , GAM , GAM , GAM , GAM , GAM , GAM , GAM , **')
     },305);
 })
 
